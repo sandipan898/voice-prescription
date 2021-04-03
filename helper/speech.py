28f81056ad9e678735f9d953e4c6c7d3482efef1
sandipan898 <sandipan.das898@gmail.com>
2021-04-03 12:22:27 +0530

    updating functions

--- helper/speech.py
+++ helper/speech.py
@@ -1,46 +1,78 @@
 import speech_recognition as sr
-import os 
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
+# from pydub import AudioSegment
+# from pydub.silence import split_on_silence
 import espeak
-
 
 espeak.init()
 speaker = espeak.Espeak()
-# speaker.rate = 300
-# speaker.say("Faster hello world")
 
-def load_chunks(filename):
-    long_audio = AudioSegment.from_mp3(filename)
-    audio_chunks = split_on_silence(
-        long_audio, min_silence_len=1800,
-        silence_thresh=-17
-    )
-    return audio_chunks
+
+def save_information():
+    pass
+
 
 def speech_rec():
     r = sr.Recognizer()
+    text = ""
+    count = 1
+    medicines = {}
 
     with sr.Microphone() as source:
-        print("Adjusting noise ")
-        r.adjust_for_ambient_noise(source, duration=1)
-        print("Recording for 4 seconds")
-        recorded_audio = r.listen(source, timeout=6)
-        print("Done recording")       # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
         
-        try:
-            # print("Text: "+r.recognize_google(audio_text, language = 'bn-IN'))
-            # print("Text: "+r.recognize_google(audio_text))
-            text = r.recognize_google(
-                    recorded_audio, 
-                    language="en-US"
-                )
-            print("Decoded Text : {}".format(text))   
-            speaker.say(text)
-            return text
+        while(not (text.lower() == 'exit')):
+            medicine_dict = {}
+            print("Medicine-%d Information", count)            
+            count += 1
+        
+            print("Adjusting noise ")
+            r.adjust_for_ambient_noise(source, duration=1)
+            print("Medicine Name:")            
+            recorded_audio = r.listen(source, timeout=6)
+            print("Done")
+        
+            try:
+                speaker.say("Medicine name")
+                text_name = r.recognize_google(
+                        recorded_audio, 
+                        language="en-US"
+                    )
+                medicine_dict["Medicine Name"] = text_name
+                print("Decoded Text : {}".format(text))   
+                
+                # speaker.say(text)
+                # return text
 
-        except Exception as e:
+            except Exception as e:
                 print(e)
-                return "Sorry, I did not get that"
+                # return "Sorry, I did not get that"
 
-# speech_rec()
+
+
+            print("Adjusting noise ")
+            r.adjust_for_ambient_noise(source, duration=1)
+            print("Medicine Instruction:")            
+            recorded_audio = r.listen(source, timeout=6)
+            print("Done")
+        
+            try:
+                speaker.say("Medicine Instruction")
+                text_instruction = r.recognize_google(
+                        recorded_audio, 
+                        language="en-US"
+                    )
+                medicine_dict["Medicine Instruction"] = text_instruction
+                print("Decoded Text : {}".format(text_instruction))   
+                
+                # speaker.say(text)
+                # return text
+
+            except Exception as e:
+                print(e)
+                # return "Sorry, I did not get that"
+
+            medicines["medicine" + str(count)] = medicine_dict
+    
+    return medicines
+
+
+print(speech_rec())
